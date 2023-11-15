bcab85c9df92ba9f17ef1e727c6ea63d488e6145
Michał <miszalaty@gmail.com>
2023-11-15 20:34:53 +0100

    shopingListItem implement

--- src/GroceryList.Core/Entities/ShoppingListItem.cs
+++ src/GroceryList.Core/Entities/ShoppingListItem.cs
@@ -2,5 +2,15 @@ namespace GroceryList.Core.Entities;
 
 public record ShoppingListItem
 {
-    
+    public string? Name { get; set; }
+    public int? Quantity { get; set; }
+
+    public ShoppingListItem()
+    {
+        Quantity = 1;
+    }
+    public void IncrementQuantity()
+    {
+        Quantity++;
+    }
 }
