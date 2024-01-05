f81a287e7a70cf5f2ee56b17182469ccaeb6d586
Michał <miszalaty@gmail.com>
2024-01-05 19:46:58 +0100

    Postman doesn't work well

---
+++ src/GroceryList.Application/Commands/AddShoppingListItemResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryList.Application.Commands
+{
+    public class AddShoppingListItemResult
+    {
+        public bool Success { get; set; }
+        public AddShoppingListItemResult(bool success)
+        {
+            Success = success;
+        }
+    }
+}
---
+++ src/GroceryList.Application/DTOs/ShoppingListItemsDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryList.Application.DTOs
+{
+    public record ShoppingListItemsDto(string[] Items);
+}
